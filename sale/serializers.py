3552495e50dbb96bd3159a5e73e304871e18da33
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-10 15:17:16 +0430

    Add atomic transaction to save method in SaleOrderSerializer

--- sale/serializers.py
+++ sale/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from .models import SaleOrder, Seller, Transaction
@@ -9,15 +10,18 @@ class SaleOrderSerializer(serializers.ModelSerializer):
         fields = ['seller', 'phone', 'amount']
 
     def save(self):
-        seller = self.validated_data['seller']
-        phone = self.validated_data['phone']
-        amount = self.validated_data['amount']
+        with transaction.atomic():
+            seller = self.validated_data['seller']
+            phone = self.validated_data['phone']
+            amount = self.validated_data['amount']
 
-        if amount > seller.credit:
-            raise ValidationError('Not enough credit')
-        SaleOrder.objects.create(seller=seller, phone=phone, amount=amount)
-        seller.credit -= amount
-        seller.save()
+            if amount > seller.credit:
+                raise ValidationError('Not enough credit')
+            SaleOrder.objects.create(seller=seller, phone=phone, amount=amount)
+        
+            # Update seller's credit after selling the charge
+            seller.credit -= amount
+            seller.save()
 
-        # Add the order to Transactions list by creating a transaction
-        Transaction.objects.create(seller=seller, amount=amount)
+            # Add the order to Transactions list by creating a transaction
+            Transaction.objects.create(seller=seller, amount=amount)
