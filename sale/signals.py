3e1de6ac26fe0e61611e86e04caf957888947773
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-10 16:14:04 +0430

    Make the signal function a little bit cleaner

--- sale/signals.py
+++ sale/signals.py
@@ -4,17 +4,17 @@ from django.dispatch import receiver
 from .models import Transaction, CreditOrder, Transaction, Seller
 
 
-@receiver(post_save, sender=CreditOrder)
-def update_seller_credit_and_create_transaction_after_credit_order(sender, **kwargs):
-    with transaction.atomic():
-        if kwargs['created']:
-            seller = Seller.objects.get(pk=kwargs['instance'].seller.id)
-            if kwargs['instance'].order_type == 'WIT':
-                seller.credit -= kwargs['instance'].amount
-                seller.save()
-                Transaction.objects.create(seller=kwargs['instance'].seller,amount=kwargs['instance'].amount,type=kwargs['instance'].order_type)
-            else:
-                seller.credit += kwargs['instance'].amount
-                seller.save()
-                Transaction.objects.create(seller=kwargs['instance'].seller,amount=kwargs['instance'].amount,type=kwargs['instance'].order_type)
 
+@receiver(post_save, sender=CreditOrder)
+def update_seller_credit_and_create_transaction_after_credit_order(sender, instance, created, **kwargs):
+    with transaction.atomic():
+        if created:
+            seller = instance.seller
+            amount = instance.amount
+            order_type = instance.order_type
+            if order_type == 'WIT':
+                seller.credit -= amount
+            else:
+                seller.credit += amount
+            seller.save()
+            Transaction.objects.create(seller=seller, amount=amount, type=order_type)
