263d38195c2beeeef3e1e54df301a8ea883782d3
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-10 16:04:39 +0430

    Update seller's credit and create transaction in one signal

--- sale/signals.py
+++ sale/signals.py
@@ -4,13 +4,15 @@ from .models import Transaction, CreditOrder, Transaction, Seller
 
 
 @receiver(post_save, sender=CreditOrder)
-def update_seller_credit_after_credit_order(sender, **kwargs):
+def update_seller_credit_and_create_transaction_after_credit_order(sender, **kwargs):
     if kwargs['created']:
         seller = Seller.objects.get(pk=kwargs['instance'].seller.id)
         if kwargs['instance'].order_type == 'WIT':
             seller.credit -= kwargs['instance'].amount
             seller.save()
+            Transaction.objects.create(seller=kwargs['instance'].seller,amount=kwargs['instance'].amount,type=kwargs['instance'].order_type)
         else:
             seller.credit += kwargs['instance'].amount
             seller.save()
+            Transaction.objects.create(seller=kwargs['instance'].seller,amount=kwargs['instance'].amount,type=kwargs['instance'].order_type)
 
