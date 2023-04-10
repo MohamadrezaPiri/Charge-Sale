748623f2149dc35f87a04dd120e2e5ff4654f165
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-10 16:06:22 +0430

    Add atomic transaction to the signal

--- sale/signals.py
+++ sale/signals.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Transaction, CreditOrder, Transaction, Seller
@@ -5,14 +6,15 @@ from .models import Transaction, CreditOrder, Transaction, Seller
 
 @receiver(post_save, sender=CreditOrder)
 def update_seller_credit_and_create_transaction_after_credit_order(sender, **kwargs):
-    if kwargs['created']:
-        seller = Seller.objects.get(pk=kwargs['instance'].seller.id)
-        if kwargs['instance'].order_type == 'WIT':
-            seller.credit -= kwargs['instance'].amount
-            seller.save()
-            Transaction.objects.create(seller=kwargs['instance'].seller,amount=kwargs['instance'].amount,type=kwargs['instance'].order_type)
-        else:
-            seller.credit += kwargs['instance'].amount
-            seller.save()
-            Transaction.objects.create(seller=kwargs['instance'].seller,amount=kwargs['instance'].amount,type=kwargs['instance'].order_type)
+    with transaction.atomic():
+        if kwargs['created']:
+            seller = Seller.objects.get(pk=kwargs['instance'].seller.id)
+            if kwargs['instance'].order_type == 'WIT':
+                seller.credit -= kwargs['instance'].amount
+                seller.save()
+                Transaction.objects.create(seller=kwargs['instance'].seller,amount=kwargs['instance'].amount,type=kwargs['instance'].order_type)
+            else:
+                seller.credit += kwargs['instance'].amount
+                seller.save()
+                Transaction.objects.create(seller=kwargs['instance'].seller,amount=kwargs['instance'].amount,type=kwargs['instance'].order_type)
 
