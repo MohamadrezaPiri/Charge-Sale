0d1c49c80dce39297573dff522c14d159ed99b94
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-11 12:43:44 +0430

    define the Transaction model as the child class of the Order model

--- sale/models.py
+++ sale/models.py
@@ -45,30 +45,14 @@ class Order(models.Model):
 
 
 
-class Transaction(models.Model):
-    TYPE_DEPOSIT = 'DEP'
-    TYPE_WITHDRAW = 'WIT'
+class Transaction(Order):
     TYPE_SALE = 'SALE'
 
-    STATUS_COMPLETED='COM'
-    STATUS_FAILED='FAI'
-
-    TYPE_CHOICES = [
-        (TYPE_DEPOSIT, 'Deposit'),
-        (TYPE_WITHDRAW, 'Withdraw'),
-        (TYPE_SALE, 'Sale')
+    TYPE_CHOICES = Order.TYPE_CHOICES + [
+        (TYPE_SALE,'Sale')
     ]
-
-    STATUS_CHOICES=[
-        (STATUS_COMPLETED,'Completed'),
-        (STATUS_FAILED,'Failed')
-    ]
-
-
-    seller = models.ForeignKey(Seller, on_delete=models.PROTECT)
-    amount = models.PositiveIntegerField()
+   
     type=models.CharField(max_length=4,choices=TYPE_CHOICES,default=TYPE_SALE)
-    status=models.CharField(max_length=3,choices=STATUS_CHOICES,default=STATUS_COMPLETED)
 
 
 class CreditOrder(models.Model): 
