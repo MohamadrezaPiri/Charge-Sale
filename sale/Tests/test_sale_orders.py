016187ee50f27f85e7c4c9c466a84938668d0e2a
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-17 16:42:40 +0430

    Update test_if_credit_is_not_enough_returns_400

--- sale/Tests/test_sale_orders.py
+++ sale/Tests/test_sale_orders.py
@@ -33,17 +33,12 @@ class TestCreateSaleOrder:
         assert seller.credit == (initial_credit - data['amount'])
 
 
-    @pytest.mark.parametrize("i", range(10))
-    @pytest.mark.xfail(reason="Possible thread safety issue")
-    def test_if_credit_is_not_enough_returns_400(self, create_sale_order, i):
-        pytest.xfail("Possible thread safety issue")
-
-        with self.lock:
-            seller=baker.make(Seller, credit=2000)
-            data = {
-                    'seller': seller.id,
-                    'phone': '09029813840',
-                    'amount': 3000
-                }
-            response = create_sale_order(data)
-            assert response.status_code == status.HTTP_400_BAD_REQUEST
+    def test_if_credit_is_not_enough_returns_400(self, create_sale_order):
+        seller=baker.make(Seller, credit=2000)
+        data = {
+            'seller': seller.id,
+            'phone': '09029813840',
+            'amount': 3000
+        }
+        response = create_sale_order(data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
