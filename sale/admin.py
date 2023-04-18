fa6fc16e28b8a6bdb87ec1a6226759feaf1e5732
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-18 22:52:08 +0430

    Set list_per_page to 10

--- sale/admin.py
+++ sale/admin.py
@@ -11,11 +11,13 @@ class SellerAdmin(admin.ModelAdmin):
     fields = ['name']
     search_fields=['name']
     list_filter=['name']
+    list_per_page = 10
 
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ['seller', 'amount','status']
     search_fields=['seller']
+    list_per_page = 10
     
 
 @admin.register(CreditOrder)
@@ -23,9 +25,11 @@ class CreditOrderAdmin(admin.ModelAdmin):
     list_display = ['seller', 'amount', 'order_type','status']
     search_fields=['seller']
     autocomplete_fields=['seller']
+    list_per_page = 10
 
 
 @admin.register(SaleOrder)
 class SaleOrderAdmin(admin.ModelAdmin):
     list_display=['seller','phone','amount']
     autocomplete_fields=['seller']
+    list_per_page = 10
