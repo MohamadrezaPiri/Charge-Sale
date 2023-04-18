576aadd9b5a7f00ae2d3714514da896908462047
MohamadrezaPiri <piry.mreza@gmail.com>
2023-04-18 22:59:15 +0430

    Remove unnecessary imports

--- sale/signals.py
+++ sale/signals.py
@@ -1,7 +1,7 @@
 from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Transaction, CreditOrder, Transaction, Seller,SaleOrder
+from .models import Transaction, CreditOrder, Transaction
 
 
 
