9ce8b498f54fab61ed99ad8ceb953760a7d84707
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-09 11:27:45 +0430

    Create TransactionCountFilter

--- sale/filters.py
+++ sale/filters.py
@@ -1,5 +1,7 @@
+from typing import Any, List, Optional, Tuple
 from django.contrib import admin
 from django.db.models import Count
+from django.db.models.query import QuerySet
 
 class CreditFilter(admin.SimpleListFilter):
     title = 'Credit'
@@ -17,3 +19,22 @@ class CreditFilter(admin.SimpleListFilter):
             return queryset.filter(credit=0)
         elif self.value() == '0<':
             return queryset.filter(credit__gt=0)
+        
+
+class TransactionCountFilter(admin.SimpleListFilter):
+    title = 'Transaction count'
+    parameter_name = 'Transactions'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('=0', 'Without transaction'),
+            ('0<', 'With transaction')
+
+        ]        
+    
+    def queryset(self, request, queryset):
+        annotated_value = queryset.annotate(transactions_count = Count('transaction'))
+        if self.value() == '=0':
+            return annotated_value.filter(transactions_count = 0)
+        elif self.value() == '0<':
+            return annotated_value.filter(transactions_count__gt = 0)
