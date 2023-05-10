ad4ae2ae3b3268bb27aa633c1c36471dfa0d7f0f
MohamadrezaPiri <piry.mreza@gmail.com>
2023-05-10 14:35:40 +0430

    Create TransactionTypeFilter class in filters.py file

--- sale/filters.py
+++ sale/filters.py
@@ -38,3 +38,21 @@ class TransactionCountFilter(admin.SimpleListFilter):
             return annotated_value.filter(transactions_count = 0)
         elif self.value() == '0<':
             return annotated_value.filter(transactions_count__gt = 0)
+        
+
+class TransactionTypeFilter(admin.SimpleListFilter):
+    title = 'Type'
+    parameter_name = 'amount'
+
+    def lookups(self, request: Any, model_admin: Any) -> List[Tuple[Any, str]]:
+        return [
+            ('0<','Deposit'),
+            ('<0','Withdraw')
+        ]
+    
+    def queryset(self, request, queryset):
+        if self.value() == '0<':
+            return queryset.filter(amount__gt = 0)
+        elif self.value() == '<0':
+            return queryset.filter(amount__lt = 0)
+
